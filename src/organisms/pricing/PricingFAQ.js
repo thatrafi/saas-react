4f20276bfe7ab104554b1ec3323d7528435052c9
thatrafi <muhammad.rafiudin@students.amikom.ac.id>
2022-04-01 16:51:37 +0800

    finish pricing page

---
+++ src/organisms/pricing/PricingFAQ.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Card from '../../atoms/card/Card';
+import Row from '../../atoms/row/Row';
+import QuestionItem from '../../molecules/question/QuestionItem';
+import styles from './PricingFAQ.module.css';
+
+const PricingFAQ = () => {
+  return (
+    <Row label="Pricing FAQ">
+      <div className={styles.pricingFAQWrapper}>
+        <Card className="faq rounded violet">
+          <div className={styles.contentFAQWrapper}>
+            <div className={styles.contentTitle}>
+              <h2>Frequently Asked Questions</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                tempor invidunt.
+              </p>
+            </div>
+            <div className={styles.contentQuestions}>
+              <QuestionItem />
+              <QuestionItem />
+              <QuestionItem />
+              <QuestionItem />
+              <QuestionItem />
+            </div>
+          </div>
+        </Card>
+      </div>
+    </Row>
+  );
+};
+
+export default PricingFAQ;
--- src/pages/Pricing/PricingPage.js
+++ src/pages/Pricing/PricingPage.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Row from '../../atoms/row/Row';
+import CTA from '../../organisms/cta/CTA';
+import PricingFAQ from '../../organisms/pricing/PricingFAQ';
 import PricingPlan from '../../organisms/pricing/PricingPlan';
 
 const PricingPage = () => {
-  return <PricingPlan />;
+  return (
+    <Row>
+      <PricingPlan />
+      <PricingFAQ />
+      <CTA />
+    </Row>
+  );
 };
 
 export default PricingPage;
