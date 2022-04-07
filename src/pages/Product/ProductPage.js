26913af7358d53b46cefc4dd7b9be7d87cd2b182
thatrafi <muhammad.rafiudin@students.amikom.ac.id>
2022-04-07 14:40:02 +0700

    FT-17 Product Icons Init

--- src/pages/Product/ProductPage.js
+++ src/pages/Product/ProductPage.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import Row from '../../atoms/row/Row';
 import Logos from '../../molecules/logos/Logos';
 import HeaderProduct from '../../organisms/product/HeaderProduct';
+import ProductFeatures from '../../organisms/product/ProductFeatures';
 
 const ProductPage = () => {
   return (
@@ -10,6 +11,7 @@ const ProductPage = () => {
       <Row className="fit">
         <Logos />
       </Row>
+      <ProductFeatures />
     </Fragment>
   );
 };
