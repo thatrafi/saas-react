3df2d5099f9b4ed628e96a578725f76095df1ef1
thatrafi <muhammad.rafiudin@students.amikom.ac.id>
2022-04-08 21:32:24 +0700

    FT-20 Finish Contact Page

---
+++ src/atoms/input/TextArea.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Input.module.css';
+import { MapPropsToStyles } from '../../helper/MapPropsToStyles';
+
+const TextArea = React.forwardRef((props, ref) => {
+  const { name, value, placeholder, className, onChange, onClick } = props;
+  const classes = MapPropsToStyles(styles, className);
+  return (
+    <textarea
+      name={name}
+      placeholder={placeholder}
+      className={classes}
+      onChange={onChange}
+      onClick={onClick}
+      ref={ref}>
+      {value}
+    </textarea>
+  );
+});
+
+export default TextArea;
---
+++ src/data/contactData.js
@@ -0,0 +1,17 @@
+export const contactData = {
+  phone: {
+    subtitle: 'Free support',
+    label: '+ 1800 145 276',
+    icon: 'Telephone.png'
+  },
+  mainEmail: {
+    subtitle: 'Help email support',
+    label: 'finsweet@gmail.com',
+    icon: 'Prism.png'
+  },
+  salesEmail: {
+    subtitle: 'Sales Enquiry',
+    label: 'sales@finsweet.com',
+    icon: 'Mail.png'
+  }
+};
