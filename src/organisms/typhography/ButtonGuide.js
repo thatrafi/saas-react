d5146728a530c02f4034015de7cd794ba80084a5
rafi <muhammad.rafiudin@students.amikom.ac.id>
2022-04-13 16:40:12 +0700

    FT-36 Finish typography

---
+++ src/organisms/typhography/ButtonGuide.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from '../../atoms/button/Button';
+import Row from '../../atoms/row/Row';
+import styles from './ButtonGuide.module.css';
+
+const ButtonGuide = () => {
+  return (
+    <Row className="fit">
+      <h4>Buttons</h4>
+      <div className={styles.buttons}>
+        <div className="container">
+          <p>Normal</p>
+          <Button label="Button" className="primary rounded" type="button" />
+        </div>
+        <div className="container">
+          <p>Hover</p>
+          <Button label="Button Hover" className="hover big rounded" type="button" />
+        </div>
+        <div className="container">
+          <p>Normal</p>
+          <Button label="Button" className="secondary rounded" type="button" />
+        </div>
+        <div className="container">
+          <p>Hover</p>
+          <Button label="Button Hover" className="primary rounded big" type="button" />
+        </div>
+      </div>
+    </Row>
+  );
+};
+
+export default ButtonGuide;
--- src/pages/Typography/TypographyPage.js
+++ src/pages/Typography/TypographyPage.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import ButtonGuide from '../../organisms/typhography/ButtonGuide';
 import ColorPallete from '../../organisms/typhography/ColorPallete';
 import TextGuide from '../../organisms/typhography/TextGuide';
 
@@ -7,6 +8,7 @@ const TypographyPage = () => {
     <Fragment>
       <TextGuide />
       <ColorPallete />
+      <ButtonGuide />
     </Fragment>
   );
 };
