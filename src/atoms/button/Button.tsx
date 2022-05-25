b45d00367da3dd54628ae5185cc1e4f459ad0c7f
rafi <muhammad.rafiudin@students.amikom.ac.id>
2022-05-25 20:00:40 +0800

    FT-49 first init

--- src/atoms/button/Button.tsx
+++ src/atoms/button/Button.tsx
@@ -3,8 +3,25 @@ import { MapPropsToStyles } from 'helper/MapPropsToStyles';
 
 import styles from 'atoms/button/Button.module.scss';
 
-const Button = (props) => {
-  const { type, className, label, onClick, onClickStyle, clicked, onChange } = props;
+export interface Props {
+  type?: 'submit' | 'reset' | 'button';
+  className?: string;
+  label?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  onClickStyle?: string;
+  clicked?: boolean;
+  onChange?: React.FormEventHandler<HTMLButtonElement>;
+}
+
+const Button: React.FC<Props> = ({
+  type,
+  className,
+  label,
+  onClick,
+  onClickStyle,
+  clicked,
+  onChange
+}) => {
   const styleButton = {};
   const classes = MapPropsToStyles(styles, className);
   const onClickClasses = MapPropsToStyles(styles, onClickStyle);
